4b70d21826264cc47c7c4a12d9e2f0e695900d11
takayuki-hirabayashi <takayuki.hirabayashi@neumann.tokyo>
2023-02-13 12:01:58 +0900

    connect news api

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
-import 'dart:io';
-
 import 'package:dio/dio.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:share_plus/share_plus.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,128 +10,35 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: PixabayPage(),
+    return const MaterialApp(
+      home: NewsPage(),
     );
   }
 }
 
-class PixabayPage extends StatefulWidget {
-  const PixabayPage({super.key});
+class NewsPage extends StatefulWidget {
+  const NewsPage({super.key});
 
   @override
-  State<PixabayPage> createState() => _PixabayPageState();
+  State<NewsPage> createState() => _NewsPageState();
 }
 
-class _PixabayPageState extends State<PixabayPage> {
-  //初めはからのリストを入れておく
-  List<PixabayImage> pixabayImages = [];
-
-  ///WebAPIを通して画像を取得する
-  Future<void> fetchImages(String text) async {
-    final response = await Dio().get(
-        'https://pixabay.com/api/?key=33501763-767244a26301fcc2882f911de&q=$text&image_type=photo&per_page=100');
-    final List hits = response.data['hits'];
-    pixabayImages = hits.map(
-      (e) {
-        return PixabayImage.fromMap(e);
-      },
-    ).toList();
-    setState(() {});
-  }
-
-  ///画像をシェアする
-  Future<void> shareImage(String url) async {
-    //1. URLから画像をダウンロード
-    final response = await Dio().get(
-      url,
-      options: Options(responseType: ResponseType.bytes),
-    );
-
-    //2. ダウンロードしたデータをファイルに保存
-    final dir = await getTemporaryDirectory();
-    final file =
-        await File(dir.path + '/image.png').writeAsBytes(response.data);
-
-    //3. Shareパッケージを呼び出して共有
-    Share.shareFiles([file.path]);
+class _NewsPageState extends State<NewsPage> {
+  Future<void> fetchImages() async {
+    Response response = await Dio().get(
+        'https://newsapi.org/v2/everything?q=apple&from=2023-02-12&to=2023-02-12&sortBy=popularity&apiKey=3a6625b8dff64baeb4e049bd17a501fc');
+    int totalResults = response.data['totalResults'];
+    print(totalResults);
   }
 
   @override
   void initState() {
     super.initState();
-    //最初に一度だけ呼ばれる
-    fetchImages('花');
+    fetchImages();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: TextFormField(
-          initialValue: '花',
-          decoration: const InputDecoration(
-            fillColor: Colors.white,
-            filled: true,
-          ),
-          onFieldSubmitted: (text) {
-            fetchImages(text);
-          },
-        ),
-      ),
-      body: GridView.builder(
-        gridDelegate:
-            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-        itemCount: pixabayImages.length,
-        itemBuilder: (context, index) {
-          final pixabayImage = pixabayImages[index];
-          return InkWell(
-            onTap: () async {
-              shareImage(pixabayImage.webformatURL);
-            },
-            child: Stack(
-              fit: StackFit.expand,
-              children: [
-                Image.network(
-                  pixabayImage.previewURL,
-                  fit: BoxFit.cover,
-                ),
-                Align(
-                    alignment: Alignment.bottomRight,
-                    child: Container(
-                        color: Colors.white,
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            const Icon(Icons.thumb_up_alt_outlined, size: 14),
-                            Text('${pixabayImage.likes}'),
-                          ],
-                        )))
-              ],
-            ),
-          );
-        },
-      ),
-    );
-  }
-}
-
-class PixabayImage {
-  final String webformatURL;
-  final String previewURL;
-  final int likes;
-
-  PixabayImage({
-    required this.webformatURL,
-    required this.previewURL,
-    required this.likes,
-  });
-
-  factory PixabayImage.fromMap(Map<String, dynamic> map) {
-    return PixabayImage(
-      webformatURL: map['webformatURL'],
-      previewURL: map['previewURL'],
-      likes: map['likes'],
-    );
+    return Scaffold();
   }
 }
